c3b6db7aafa511132ae04a9f6acb73d12cf3ecae
Chanon <ccionflare090290@gmail.com>
2019-11-13 15:04:35 +0700

    init web service kotlin for raspberry pi

---
+++ src/main/kotlin/com/sample/Wserv/controller/WservController.kt
@@ -0,0 +1,46 @@
+package com.sample.Wserv.controller
+
+
+//import com.sample.sample.service.SampleService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.atomic.AtomicLong
+
+@Controller
+class SampleController(){
+
+    @GetMapping("/")
+    fun index(model: Model): String  {
+        // 以下３行とも同じように動く
+        // model.addAttribute("modelList", service.find())
+        // model.set("modelList", service.find())
+        //model["modelList"] = service.find()
+        return "index"
+    }
+}
+@Controller
+class Sample2Controller(){
+
+    @GetMapping("/test")
+    fun index(model: Model): String  {
+        return "test"
+    }
+}
+
+data class Greeting(val id: Long, val content: String)
+
+@RestController
+class GreetingController {
+
+
+    val counter = AtomicLong()
+
+    @GetMapping("/greeting")
+    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) =
+            Greeting(counter.incrementAndGet(), "Hello, $name")
+
+}
---
+++ src/test/kotlin/com/sample/Wserv/WservApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sample.Wserv
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WservApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
